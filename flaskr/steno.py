e092e28339e69be389f2d6bc11c3bc70344392f0
shaxl <actionplan2456@protonmail.com>
2022-04-05 01:58:21 -0500

    Fixed splash page showing post data

--- flaskr/steno.py
+++ flaskr/steno.py
@@ -12,19 +12,19 @@ bp = Blueprint('steno', __name__)
 
 @bp.route('/')
 def index():
+    """Index page for users who are not logged in."""
     return render_template('steno/index.html')
 
 @bp.route('/splash')
 @login_required
 def splash():
     db = get_db()
-    steno = db.execute(
-        'SELECT p.id, title, body, created, owner_id, username'
-        ' FROM steno p JOIN user u ON p.owner_id = u.id'
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
-    return render_template('steno/splash.html', steno=steno)
-
+    return render_template('steno/splash.html', posts=posts)
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
@@ -42,7 +42,7 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO steno (title, body, owner_id)'
+                'INSERT INTO post (title, body, author_id)'
                 ' VALUES (?, ?, ?)',
                 (title, body, g.user['id'])
             )
@@ -51,18 +51,18 @@ def create():
 
     return render_template('steno/create.html')
 
-def get_steno(id, check_author=True):
-    steno = get_db().execute(
-        'SELECT p.id, title, body, created, owner_id, username'
-        ' FROM steno p JOIN user u ON p.owner_id = u.id'
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
     ).fetchone()
 
-    if steno is None:
+    if post is None:
         abort(404, f"Message id {id} doesn't exist.")
 
-    if check_author and steno['owner_id'] != g.user['id']:
+    if check_author and post['author_id'] != g.user['id']:
         abort(403)
 
-    return steno
+    return post
